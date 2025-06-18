0d52089314d9465750017c0fec809b270ee763dc
Sergey Lysenko <sslysenko19787@gmail.com>
2025-06-19 02:02:52 +0300

    Add first views and components 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/assets/base.css 	deleted:    src/assets/logo.svg 	modified:   src/assets/main.css 	new file:   src/components/CreateNewListModal.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/ShoppingListCard.vue 	deleted:    src/components/TheWelcome.vue 	deleted:    src/components/WelcomeItem.vue 	deleted:    src/components/icons/IconCommunity.vue 	deleted:    src/components/icons/IconDocumentation.vue 	deleted:    src/components/icons/IconEcosystem.vue 	deleted:    src/components/icons/IconSupport.vue 	deleted:    src/components/icons/IconTooling.vue 	modified:   src/main.ts 	modified:   src/router/index.ts 	deleted:    src/views/AboutView.vue 	modified:   src/views/HomeView.vue

---
+++ src/components/CreateNewListModal.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits(["submit", "onCreateNewList"]);
+
+const newListName = ref("");
+
+function createNewList() {
+  emit("onCreateNewList", {
+    name: newListName.value,
+  });
+  reset();
+}
+
+function reset() {
+  newListName.value = "";
+}
+</script>
+
+<template>
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Create New List</h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              class="form-control"
+              aria-label="new list name"
+              aria-describedby="inputGroup-sizing-default"
+              placeholder="New List"
+              v-model="newListName"
+            />
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-dismiss="modal"
+            @click="createNewList"
+          >
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+import ShoppingListCard from "../components/ShoppingListCard.vue";
+import CreateNewListModal from "../components/CreateNewListModal.vue";
+
+const title = ref("Your Shopping Lists");
+const lists = ref([]);
+function createNewList(event) {
+  lists.value.push(event);
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container-xl">
+    <div>
+      {{ title }}
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Create new list
+      </button>
+    </div>
+    <CreateNewListModal @onCreateNewList="createNewList" />
+    <div v-for="list in lists" :key="list.name">
+      <ShoppingListCard :data="list" />
+    </div>
   </main>
 </template>
